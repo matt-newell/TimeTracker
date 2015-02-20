20324fb90e526325f93d9cc7e0d5758bd5dd0f04
matt-newell <matt.newell@codescience.com>
2015-02-20 09:06:08 -0500

    Adding notes

--- spa/timeTracker/src/app/time/time.controller.js
+++ spa/timeTracker/src/app/time/time.controller.js
@@ -3,26 +3,30 @@
 angular.module('timeTracker')
   .controller('TimeCtrl', function ($scope) {
   	$scope.imagePath = configSettings.staticPath + '/assets/images/';
-	$scope.minDate = new Date();
+    $scope.minDate = new Date();
   	$scope.selectedProject = undefined;
   	$scope.selectedTask = undefined;
-  	$scope.projectData = [
+  	
+    $scope.projectData = [
   		{name:"GenTux", hours: 100, remaining: 50},
   		{name:"Demandbase", hours: 250, remaining: 150},
   		{name:"Health1", hours: 1000, remaining: 375}
   	];
-  	$scope.taskData = [
+  	
+    $scope.taskData = [
   		{name:"Development", hours: 100, remaining: 50},
   		{name:"Project Managment", hours: 250, remaining: 150},
   		{name:"No Ideal", hours: 1000, remaining: 375}
   	];
   	$scope.timeEntries = [];
-  	$scope.pushTime = function(){
+  	
+    $scope.pushTime = function(){
   		var time = {
   			project: $scope.selectedProject,
   			task: $scope.selectedTask,
   			date: $scope.dt,
-  			hours: $scope.hours
+  			hours: $scope.hours,
+        notes: $scope.notes
   		};
   		$scope.timeEntries.push(time);
   	};
